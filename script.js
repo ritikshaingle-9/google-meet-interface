766155e2b6ab9f1d1f665857caf98997b7128adc
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2024-12-29 15:02:32 +0530

    js added

---
+++ script.js
@@ -0,0 +1,23 @@
+const student = ["Nidhi","Mannat","Sandhya","Vikas","Shourya","Nishant","Pranav","Sujata","Snehal","Piyara","Shobha","Krish","Vidhi"]
+const studentContainer= document.getElementById(`card-container`);
+
+function renderstudents(studentlist){
+    studentContainer.innerHTML='';
+
+    studentlist.forEach((students)=>{
+      studentContainer.innerHTML +=`<div class='card'>${students}</div>` 
+    })
+}
+renderstudents(student);
+
+function loadSearchResult(){
+    const inputElement = document.getElementById(`search`);
+    const searchText = inputElement.value.tolowerCase();
+
+    const filteredStudents = student.filter((students)=>{
+      if(students.toLowerCase().includes(searchText)){
+        return true;
+      }
+    })
+    renderstudents(filteredStudents);
+}
